a67e18a775863a6441e965f14bd240621c09acc0
Julio Sanjuan <julio.sanjuan@konfio.mx>
2022-11-03 14:42:46 -0600

    feature: support for pagination
    Now accepts page and size query params to fetch paginated results.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyparser = require('body-parser')
 const cors = require('cors')
 const ApiResponse = require('./entity/api.response')
 const ProductsService = require('./service/products.service')
+const PaginationTool = require('./tools/pagination.tool')
 
 const port = process.env.PORT || 3000
 const {verifyApiKey} = require('./middleware/request.middleware')
@@ -33,11 +34,12 @@ app.get('/api/search', async (req, res) => {
     const queryString = req.query;
     const searched = queryString["query"];
     if(!searched) {
-        apiResponse.badRequest( "Yuo must specify the query parameter" ).send();
+        apiResponse.badRequest( "Yuo must specify the 'query' parameter" ).send();
         return;
     }
+    const pagination = PaginationTool.extractPaginationSetup(queryString);
     try {
-        const {done, result} = await ProductsService.search(searched);
+        const {done, result} = await ProductsService.search(searched, pagination);
         if(done) {
             apiResponse.success(result);
         } else {
